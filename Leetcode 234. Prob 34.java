a33f03e67206bf98cb6adeadf1377ac8b2b8bb52
Vanshika tiwari <107793386+vanshikatiwary@users.noreply.github.com>
2024-08-20 23:47:22 +0530

    Add files via upload

---
+++ Leetcode 234. Prob 34.java
@@ -0,0 +1,41 @@
+class Solution{
+
+  boolean isPalindrome(ListNode head) {
+
+    // Find the middle
+    ListNode fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    if (fast != null) { // odd nodes: let right half smaller
+      slow = slow.next;
+    }
+
+    // Reverse the second half
+    slow = reverseList(slow);
+    fast = head;
+
+    // Start comparing one by one
+    while (slow != null) {
+      if (fast.val != slow.val)
+        return false;
+
+      fast = fast.next;
+      slow = slow.next;
+    }
+    return true;
+  }
+
+  private ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    while (head != null) {
+      ListNode next = head.next;
+      head.next = prev;
+      prev = head;
+      head = next;
+    }
+    return prev;
+  }
+
+}
