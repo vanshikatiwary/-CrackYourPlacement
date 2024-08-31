1ecdde7d6de29e61c06815f0e5e4d685d0eed878
Vanshika tiwari <107793386+vanshikatiwary@users.noreply.github.com>
2024-08-31 23:57:11 +0530

    Add files via upload

---
+++ Leetcode 173. Prob 67.java
@@ -0,0 +1,29 @@
+public class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+    
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode tmpNode = stack.pop();
+        pushAll(tmpNode.right);
+        return tmpNode.val;
+    }
+    private void pushAll(TreeNode node) {
+    while (node != null) {
+        stack.push(node);
+        node = node.left;
+    }
+    }
+ }
+
+
+    
+   
